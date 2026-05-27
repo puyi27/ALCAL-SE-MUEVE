62060f42b09c325998872245422f002a0c9aab03
ÁngelPostigo <angelposrod@gmail.com>
2026-05-27 15:20:36 +0200

    Fix: Restablecer scroll al inicio (top) al cambiar de página en el Router

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import CustomCursor from "./CustomCursor";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Layout() {
   return (
     <div className="bg-[#011B11] text-[#FBF5E9] font-sans selection:bg-[#117C4E] selection:text-white md:cursor-none overflow-x-hidden min-h-screen flex flex-col">
+      <ScrollToTop />
       <CustomCursor />
       <Navigation />
       <main className="flex-grow">
